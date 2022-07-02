01a16292f37ce6177b228579c97e031ed2e3508e
neefrankie <neefrankie@gmail.com>
2022-07-02 20:25:51 +0800

    fix: removing logging

--- sorting/insertion_sort.go
+++ sorting/insertion_sort.go
@@ -1,26 +1,19 @@
 package sorting
 
-import "fmt"
-
 func InsertionSort[T Number](arr []T) []T {
 
 	for i := 1; i < len(arr); i++ {
-		fmt.Printf("Start outer loop %d\n", i)
 
 		var temp = arr[i]
-		fmt.Printf("Taken out element at %d:%d\n", i, arr[i])
 
 		var j = i
-		fmt.Printf("\tInner loop starts at %d\n", j)
+
 		for j > 0 && arr[j-1] >= temp {
-			fmt.Printf("\tMove element at %d to %d\n", j-1, j)
 
 			arr[j] = arr[j-1]
 			j--
 		}
-		fmt.Printf("Put slotted element %d back to %d\n", temp, j)
 		arr[j] = temp
-		fmt.Printf("End outer loop %d\n\n", i)
 	}
 
 	return arr
