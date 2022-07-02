84cad5395fc1e71f807fa21d620e8e881bfd74e1
neefrankie <neefrankie@gmail.com>
2022-07-02 12:17:42 +0800

    implement question 6-10 of section 3.1

--- dma/section_3_1.go
+++ dma/section_3_1.go
@@ -1,5 +1,7 @@
 package dma
 
+import "strings"
+
 // This file answers questions of Exercise 3.1 of
 // Discrete Mathematics and Its Applications.
 
@@ -71,4 +73,91 @@ func FindDuplicates(arr []int) []int {
 	}
 
 	return items
+}
+
+// CountNegativeInts implements question 6:
+// Takes as input a list of n integers and finds the
+// number of negative integers in the list.
+func CountNegativeInts(ints []int) int {
+	var count int
+
+	for _, n := range ints {
+		if n < 0 {
+			count++
+		}
+	}
+
+	return count
+}
+
+// LastEvenInt implements question 7:
+// Takes as input a list of n integers
+// and find the location of the last even integer in the list
+// or returns -1 if there are no event integers in the list.
+func LastEvenInt(arr []int) int {
+	var pos int = -1
+	for i, n := range arr {
+		if n%2 == 0 {
+			pos = i
+		}
+	}
+
+	return pos
+}
+
+// IsPalindrome implements question 9 to test if a string is a palindrome.
+// A palindrome is a string that reads the same forward and backward. For example:
+// A man, a plan, a canal, Panama!
+func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	var left = 0
+	var right = len(s) - 1
+
+	for left < right {
+		leftCh := s[left]
+		rightCh := s[right]
+		if !isLetter(leftCh) {
+			left++
+			continue
+		}
+		if !isLetter(rightCh) {
+			right--
+			continue
+		}
+		if leftCh != rightCh {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
+
+func isLetter(ch byte) bool {
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')
+}
+
+// XPower implements question 10 to compute x^n,
+// where x is a real number and n is an integer.
+func XPower(x float64, n int) float64 {
+	if n >= 0 {
+		return xPower(x, n)
+	}
+
+	return 1 / xPower(x, -n)
+}
+
+func xPower(x float64, n int) float64 {
+	if n < 0 {
+		panic("n must be non-negative")
+	}
+	var result float64 = 1
+
+	for i := 1; i <= n; i++ {
+		result = result * x
+	}
+
+	return result
 }
--- dma/section_3_1_test.go
+++ dma/section_3_1_test.go
@@ -82,3 +82,124 @@ func TestFindDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func TestCountNegativeInts(t *testing.T) {
+	type args struct {
+		ints []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Count Negative",
+			args: args{
+				ints: []int{10, -8, 4, 2, -13, -15, -6},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CountNegativeInts(tt.args.ints); got != tt.want {
+				t.Errorf("CountNegativeInts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLastEvenInt(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Last event position",
+			args: args{
+				arr: []int{2, 3, 4, 5, 6, 7},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LastEvenInt(tt.args.arr); got != tt.want {
+				t.Errorf("LastEvenInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Is palindrome",
+			args: args{
+				s: "A man, a plan, a canal, Panama!",
+			},
+			want: true,
+		},
+		{
+			name: "Not palindrome",
+			args: args{
+				s: "Hello, world",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("IsPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXPower(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "Positive power",
+			args: args{
+				x: 2,
+				n: 3,
+			},
+			want: 8,
+		},
+		{
+			name: "Negative power",
+			args: args{
+				x: 2,
+				n: -3,
+			},
+			want: 0.125,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := XPower(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("XPower() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
